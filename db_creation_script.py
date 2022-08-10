b4e0012bb1f6840665d96b60223d242d006fe1bb
Yarin cohen <yarin.cohen999@gmail.com>
2022-08-10 11:18:16 +0300

    Minor fix

--- db_creation_script.py
+++ db_creation_script.py
@@ -3,5 +3,4 @@ from sqlalchemy import create_engine
 from tables.const import BASE
 
 engine = create_engine("postgresql://postgres:postgrespw@localhost:55000")
-BASE.metadata.__videos__.drop()
-#BASE.metadata.create_all(engine)
+BASE.metadata.create_all(engine)
