ded9ba53dd430eb032fd262b5fbcb337588d1900
Yarin cohen <yarin.cohen999@gmail.com>
2022-08-10 13:30:49 +0300

    Added missing imports

--- db_creation_script.py
+++ db_creation_script.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
 # local imports
+from tables.videos import Videos
+from tables.metadata import Metadata
+from tables.frames import Frames
 from tables.const import BASE
 
 engine = create_engine("postgresql://postgres:postgrespw@localhost:55000")
